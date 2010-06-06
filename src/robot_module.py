ab0f6bb175dde6b6e726a24d5830b55c780871c8
bamboor <bamboor@3a166271-741d-0410-a5f9-4fe1b9ccf291>
2010-06-06 09:31:27 +0000

    Update project

--- src/robot_module.py
+++ src/robot_module.py
@@ -12,26 +12,34 @@ import os
 import sys
 
 def main(url, out_folder="C:/test/"):
-    """Downloads all the images at 'url' to /test/"""
-    soup = bs(urlopen(url))
-    parsed = list(urlparse.urlparse(url))
-
-    for image in soup.findAll("img"):
-        try:
-            print "Image: %(src)s" % image
-            filename = image["src"].split("/")[-1]
-            parsed[2] = image["src"]
-            outpath = os.path.join(out_folder, filename)
-            urlretrieve(urlparse.urlunparse(parsed), outpath)
-        except: pass
-
+    try:
+        """Downloads all the images at 'url' to /test/"""
+        soup = bs(urlopen(url))
+        parsed = list(urlparse.urlparse(url))
+        for image in soup.findAll("img"):
+            try:
+                print "Image: %(src)s" % image
+                filename = image["src"].split("/")[-1]
+                parsed[2] = image["src"]
+                tmp = urlopen(urlparse.urlunparse(parsed))
+                meta = tmp.info()
+                print urlparse.urlunparse(parsed)
+                print meta.getheaders("Content-Length")[0]
+                length = int(meta.getheaders("Content-Length")[0])
+                size   = 40000
+                if (length  > size) :
+                           print "start write"
+                           outpath = os.path.join(out_folder, filename)
+                           urlretrieve(urlparse.urlunparse(parsed), outpath)
+                           print "end  write"
+                else: findlink(url)
+            except:pass
+    except: pass
 
 def _usage():
     print "usage: python dumpimages.py http://example.com [outpath]"
 
-if __name__ == "__main__":
-    """url = sys.argv[-1] """
-    url="http://www.teenee.com"
+def findlink(url):
     page=urlopen(url)
     data=page.read().split("</a>")
     tag="<a href=\""
@@ -44,13 +52,31 @@ if __name__ == "__main__":
                 end=item.index(endtag)
             except: pass
             else:
-                print item[:end]
-                url2 = item[:end]
-                out_folder = "C:/test/"
-                if not url.lower().startswith("http"):
-                    out_folder = sys.argv[-1]
-                    url = sys.argv[-2]
-                    if not url.lower().startswith("http"):
-                        _usage()
-                        sys.exit(-1)
-                main(url2, out_folder)
+                try:
+                    print item[:end]
+                    url2 = item[:end]
+                    out_folder = "C:/test/"
+                    print "========="
+                    print url2
+                    print url
+                    print "========="
+                    str1 = url2.split(".")
+                    str2 = url.split(".")
+                    print str1[1]
+                    print str2[1]
+                    if not url2.lower().startswith("http"):
+                        url2 = url+url2
+                        print url2+"  =========="
+                        """ out_folder = sys.argv[-1] """
+                        """if not url.lower().startswith("http"):
+                            _usage()
+                            sys.exit(-1) """
+                    main(url2, out_folder)
+                    """ if not str1[1] == str2[1]:findlink(url2) """
+                except:pass
+
+if __name__ == "__main__":
+    """url = sys.argv[-1] """
+    urlstart="http://www.bigfoto.com/"
+    findlink("http://picasaweb.google.co.th/lh/explore")
+    findlink("http://www.flickr.com/explore/")
